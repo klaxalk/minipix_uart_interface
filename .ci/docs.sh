b1879cfdbd30365b0c51c416da495ec9e31433bd
Tomas Baca <klaxalk@gmail.com>
2021-03-18 12:08:37 +0100

    added doxygen

---
+++ .ci/docs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+sudo apt-get install doxygen doxygen-latex graphviz
+
+cd software
+
+line=$(cat CMakeLists.txt | grep "project(.*)" -o); tmp=${line:8}; proj_name=${tmp:0:${#tmp}-1};
+sed -i 's/\(PROJECT_NAME\s*=\s*\)\".*\"/\1\"'$proj_name'\"/g' Doxyfile Doxyfile;
+doxygen Doxyfile
