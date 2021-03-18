c49657b209c8fc44535b8a24eb12da95ba222176
Tomas Baca <klaxalk@gmail.com>
2021-03-18 18:39:20 +0100

    mroe documentation

--- software/README.md
+++ software/README.md
@@ -2,13 +2,13 @@
 
 ## LLCP - Low-level Communication Protocol
 
-* C header library
+* C library
 * UART link layer between MiniPIX and the example interface
 * to be used in the MiniPIX interface and in the MiniPIX itself
 
 ## MUI - MiniPIX UART Interface
 
-* C++ header library
+* C library
   * contains virtual classes to be implemented by the HW integrator
 * uses LLCP
 * interfaces with the MiniPIX over UART using
@@ -29,3 +29,11 @@ Saves incoming packeted data into files.
 ## Decoder - PC data decoder
 
 Stitches packeted data from files into viewable images.
+
+# Running in Tmux sessions
+
+`./tmux` contains tmuxinator sessions ready for testing.
+
+```bash
+sudo apt-get -y install tmux tmuxinator
+```
--- software/mui/include/mui.h
+++ software/mui/include/mui.h
@@ -12,21 +12,68 @@ extern "C" {
 #include <stdint.h>
 #include <stdbool.h>
 
+// | ---------- function pointers to user's callbacks --------- |
+
+  /**
+   * @brief Function pointer for implementation of seting an LED state.
+   *
+   * @param true = light is on, false = light is of
+   */
 typedef void (*mui_ledSetHW_t)(const bool);
+
+/**
+ * @brief Function pointer for implementaion of sending a char (byte) over
+ * a serial line to MiniPIX.
+ *
+ * @param byte to send
+ */
 typedef void (*mui_sendChar_t)(const uint8_t char_out);
+
+/**
+ * @brief Function pointer for implementation of sending a string (array of
+ * bytes) over a serial line to MiniPIX.
+ *
+ * @param str_out poiter to the first element of the byte array
+ * @param len the number of bytes to send
+ */
 typedef void (*mui_sendString_t)(const uint8_t *str_out, const uint16_t len);
-typedef void (*mui_processImagePacket_t)(const LLCP_FrameData_t *image_data);
+
+/**
+ * @brief Function pointer to user implementation of callback to process
+ * incoming Frame data.
+ *
+ * @param pointer to the structure with the data
+ */
+typedef void (*mui_processFrameData_t)(const LLCP_FrameData_t *image_data);
+
+/**
+ * @brief Function pointer to user implementation of callback to process
+ * incoming Status message.
+ *
+ * @param pointer to the structure with the data
+ */
 typedef void (*mui_processStatus_t)(const LLCP_Status_t *status_data);
+
+/**
+ * @brief Function pointer for implementation of a sleep function.
+ *
+ * @param duration time to sleep in milliseconds
+ */
 typedef void (*mui_sleepHW_t)(const uint16_t duration);
 
+/**
+ * @brief Structure containing pointers to user callbacks.
+ * User shouls provide implementations to all the listed
+ * functions.
+ */
 typedef struct
 {
-  mui_ledSetHW_t           ledSetHW;
-  mui_sendChar_t           sendChar;
-  mui_sendString_t         sendString;
-  mui_processImagePacket_t processImagePacket;
-  mui_processStatus_t      processStatus;
-  mui_sleepHW_t            sleepHW;
+  mui_ledSetHW_t         ledSetHW;
+  mui_sendChar_t         sendChar;
+  mui_sendString_t       sendString;
+  mui_processFrameData_t processFrameData;
+  mui_processStatus_t    processStatus;
+  mui_sleepHW_t          sleepHW;
 } MUI_FcnPrototypes_t;
 
 typedef struct
@@ -37,32 +84,92 @@ typedef struct
   MUI_FcnPrototypes_t fcns;
 } MUI_Handler_t;
 
-// get status, to be called by the integrator
-void getStatus(MUI_Handler_t *mui_handler);
+// --------------------------------------------------------------
+// |                           public                           |
+// --------------------------------------------------------------
 
-//}
+// | ---------------------- "constructor" --------------------- |
 
+/**
+ * @brief The "constructor" of the MUI handler. Should be called
+ * by the user just after allocating it.
+ *
+ * @param mui_handler
+ */
 void mui_initialize(MUI_Handler_t *mui_handler);
 
+// | -------------- interraction with the MiniPIX ------------- |
+
+/**
+ * @brief Command to acquire a frame over a period of the acquisition_time.
+ * As a result, the MiniPIX will start obtaning data and will call the
+ *                        processFrameData()
+ * function will be called.
+ *
+ * @param mui_handler
+ * @param acquisition_time
+ */
 void mui_measureFrame(MUI_Handler_t *mui_handler, const uint16_t acquisition_time);
 
+/**
+ * @brief Command to get the status of the MinixPIX HW. As a result,
+ * the method
+ *                          processStatus()
+ * will get called.
+ *
+ * @param mui_handle
+ */
 void mui_getStatus(MUI_Handler_t *mui_handle);
-
-void mui_sendAck(MUI_Handler_t *mui_handler, const bool success);
 
 // | ------------- UART communication with MiniPIX ------------ |
 
+/**
+ * @brief Called by the user with every incoming byte on the UART to the MiniPIX.
+ *
+ * @param mui_handler
+ * @param byte_in
+ */
 void mui_receiveCharCallback(MUI_Handler_t *mui_handler, const uint8_t byte_in);
-
-// | -------------------- LED signalization ------------------- |
-
-void mui_ledSet(MUI_Handler_t *mui_handler, const bool new_state);
-
-void mui_ledToggle(MUI_Handler_t *mui_handler);
 
 // | --------------- the main update() function --------------- |
 
+/**
+ * @brief Called by the user to led MUI do stuff in the background.
+ * Should be called regularly, e.g., in the main() loop or in a
+ * timer's callback.
+ *
+ * @param mui_handler
+ */
 void mui_update(MUI_Handler_t *mui_handler);
+
+// --------------------------------------------------------------
+// |                           private                          |
+// --------------------------------------------------------------
+
+/**
+ * @brief Sends ack to the MiniPIX. Not to be called by the user.
+ *
+ * @param mui_handler
+ * @param success
+ */
+void mui_sendAck(MUI_Handler_t *mui_handler, const bool success);
+
+// | -------------------- LED signalization ------------------- |
+
+/**
+ * @brief set LED state
+ *
+ * @param mui_handler
+ * @param new_state true = light is on, false = light is off
+ */
+void mui_ledSet(MUI_Handler_t *mui_handler, const bool new_state);
+
+/**
+ * @brief toggle the state of the LED
+ *
+ * @param mui_handler
+ */
+void mui_ledToggle(MUI_Handler_t *mui_handler);
 
 #ifdef __cplusplus
 }
